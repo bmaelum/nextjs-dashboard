6cee80b797f4d5a9ff004597efdbfbf70f8e537a
Bjørnar Mælum <bjornarmaelum@gmail.com>
2024-07-31 13:25:39 +0200

    Commented out code for seeding database to avoid duplicating

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -1,4 +1,4 @@
- import bcrypt from 'bcrypt';
+/*  import bcrypt from 'bcrypt';
  import { db } from '@vercel/postgres';
  import { invoices, customers, revenue, users } from '../lib/placeholder-data';
 
@@ -117,3 +117,4 @@ export async function GET() {
      return Response.json({ error }, { status: 500 });
    }
 }
+ */
